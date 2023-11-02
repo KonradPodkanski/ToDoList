647c985177e9d7d5339497ad632c0ed9b8113487
unknown <podkanski050@gmail.com>
2023-11-02 18:00:11 +0100

    made options for the buttons like: remove note, give possible to open task menu and shut its down

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 let optionBtn;
 let menuBtns;
 let menuSingleBtn;
+let noteSection;
+let dropDownMenu;
+let addTaskPopUp;
 const main = () => {
 	prepareDOMElements();
 	prepareDOMEvents();
@@ -9,17 +12,38 @@ const prepareDOMElements = () => {
 	optionBtn = document.querySelector(".fa-gear");
 	menuBtns = document.querySelector(".buttons");
 	menuSingleBtn = document.querySelectorAll(".menu-btn");
+	noteSection = document.querySelector(".bottom-note-section");
+	dropDownMenu = document.querySelector(".buttons");
+	addTaskPopUp = document.querySelector(".add-task-pop-up");
+};
+const prepareDOMEvents = () => {
+	optionBtn.addEventListener("click", showOptions);
+	noteSection.addEventListener("click", removeNote);
+	dropDownMenu.addEventListener("click", openSelectedOption);
+	addTaskPopUp.addEventListener("click", taskMenuEvents);
 };
 const showOptions = () => {
-	if (window.matchMedia("(max-width: 680px)").matches) {
+	if (window.matchMedia("(max-width: 740px)").matches) {
 		menuSingleBtn.forEach(e => {
 			e.classList.toggle("wrap-animation");
 		});
 		optionBtn.classList.toggle("active");
 	}
 };
-const prepareDOMEvents = () => {
-	optionBtn.addEventListener("click", showOptions);
+const removeNote = e => {
+	if (e.target.classList.contains("fa-trash")) {
+		e.target.closest(".note").remove();
+	}
 };
-
+const openSelectedOption = e => {
+	if (e.target.classList.contains("fa-plus")) {
+		addTaskPopUp.classList.add("active-pop-up");
+	}
+};
+const taskMenuEvents = e => {
+	if (e.target.classList.contains("cancel")) {
+		addTaskPopUp.classList.remove("active-pop-up");
+		showOptions();
+	}
+};
 document.addEventListener("DOMContentLoaded", main);
